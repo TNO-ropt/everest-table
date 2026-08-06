9b2f97fef3bec50c683252e77c0f9e231c0334f4
Peter Verveer <pieter.verveer@tno.nl>
2026-08-06 17:27:08 +0200

    Update for ropt module change

--- src/everest_table/_results_table.py
+++ src/everest_table/_results_table.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Final, Literal
 
-from ropt.workflow.event_handlers import DataFrameHandler
+from ropt.components.event_handlers import DataFrameHandler
 from tabulate import tabulate
 
 if TYPE_CHECKING:
